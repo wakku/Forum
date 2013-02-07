932f8f351935dafb6f42a580829ea24db5c4de57
Gorodscy <fefow@me.com>
2013-02-07 03:07:48 -0500

    CSS fix [was missing a / ]

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 http = require('http');
 fs = require('fs');
-path = 'C:/Users/Vanessa/Desktop/Courses/ProgWeb/Forum';
+path = '/Users/fernandogorodscy/Desktop/Forum.git';
 
 PORT = 30930;
 
@@ -47,7 +47,7 @@ http.createServer(function(request, response) {
     // directory.
 
 	fs = require('fs')
-	fs.readFile(path+ 'style.css', 'utf8', function (err,data) {
+	fs.readFile(path+ '/style.css', 'utf8', function (err,data) {
   		if (err) {
     		return console.log(err);
 		}
